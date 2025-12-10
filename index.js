0a3dd447e9c912490bef1c747fb14a10fea923f2
kaviyarasan <kaviyarasan.dev@gmail.com>
2025-12-10 11:18:00 +0530

    Add user route and user data module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
-
+import users from "./user.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello world server");
+});
+
+app.get("/api/user", (req, res) => {
+  res.json(users);
 });
 
 const PORT = 3000;
